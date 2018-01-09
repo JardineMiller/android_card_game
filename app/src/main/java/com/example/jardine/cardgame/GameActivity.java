5b2b21f0b40eaafe435e5c22a46ac57f1f6fa84a
Jardine <jardine.miller@gmail.com>
2018-01-09 09:17:59 +0000

    Data successfully transferring between activities

--- app/src/main/java/com/example/jardine/cardgame/GameActivity.java
+++ app/src/main/java/com/example/jardine/cardgame/GameActivity.java
@@ -4,24 +4,30 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
-    public String numberOfPlayers;
-    public String numberOfCards;
+    String numberOfPlayers;
+    String numberOfCards;
+    public EditText numPlayers;
+    public EditText numCards;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        TextView numPlayers = findViewById(R.id.num_of_players);
-        TextView numCards = findViewById(R.id.num_of_cards);
-        numberOfPlayers = numPlayers.getText().toString();
-        numberOfCards = numCards.getText().toString();
+
+        numPlayers = findViewById(R.id.num_of_players);
+        numCards = findViewById(R.id.num_of_cards);
+
     }
 
     public void onPlayButtonClick(View view) {
         Intent intent = new Intent(this, ResultActivity.class);
+        numberOfPlayers = numPlayers.getText().toString();
+        numberOfCards = numCards.getText().toString();
         intent.putExtra("numOfPlayers", numberOfPlayers);
         intent.putExtra("numOfCards", numberOfCards);
         startActivity(intent);
