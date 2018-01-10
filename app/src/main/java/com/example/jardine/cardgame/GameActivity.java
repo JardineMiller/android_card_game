c71d42ca1a0753a573ea1c539beead4c363d628d
Jardine <jardine.miller@gmail.com>
2018-01-10 12:08:07 +0000

    App fully functioning with images for cards

--- app/src/main/java/com/example/jardine/cardgame/GameActivity.java
+++ app/src/main/java/com/example/jardine/cardgame/GameActivity.java
@@ -8,6 +8,8 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class GameActivity extends AppCompatActivity {
 
     TextView playerName;
@@ -16,8 +18,19 @@ public class GameActivity extends AppCompatActivity {
     TextView computerScore;
     TextView resultText;
 
-    ImageView playerCardOne;
-    ImageView playerCardTwo;
+    ImageView playerCard1;
+    ImageView playerCard2;
+    ImageView playerCard3;
+    ImageView playerCard4;
+    ImageView playerCard5;
+    ArrayList<ImageView> playerCards;
+
+    ImageView computerCard1;
+    ImageView computerCard2;
+    ImageView computerCard3;
+    ImageView computerCard4;
+    ImageView computerCard5;
+    ArrayList<ImageView> computerCards;
 
     Button hitButton;
     Button compareButton;
@@ -33,6 +46,30 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        playerCards = new ArrayList<>();
+        computerCards = new ArrayList<>();
+
+        playerCard1 = findViewById(R.id.player_card_1);
+        playerCard2 = findViewById(R.id.player_card_2);
+        playerCard3 = findViewById(R.id.player_card_3);
+        playerCard4 = findViewById(R.id.player_card_4);
+        playerCard5 = findViewById(R.id.player_card_5);
+        playerCards.add(playerCard1);
+        playerCards.add(playerCard2);
+        playerCards.add(playerCard3);
+        playerCards.add(playerCard4);
+        playerCards.add(playerCard5);
+
+        computerCard1 = findViewById(R.id.computer_card_1);
+        computerCard2 = findViewById(R.id.computer_card_2);
+        computerCard3 = findViewById(R.id.computer_card_3);
+        computerCard4 = findViewById(R.id.computer_card_4);
+        computerCard5 = findViewById(R.id.computer_card_5);
+        computerCards.add(computerCard1);
+        computerCards.add(computerCard2);
+        computerCards.add(computerCard3);
+        computerCards.add(computerCard4);
+        computerCards.add(computerCard5);
 
         resultText = findViewById(R.id.result_text);
         resultText.setVisibility(View.GONE);
@@ -57,11 +94,15 @@ public class GameActivity extends AppCompatActivity {
         game.startGame();
         getInfo();
 
+        displayHand(game.getPlayer());
+
+
     }
 
     public void hit(View button) {
         if (game.getPlayer().handCount() < 5) {
             game.dealCards();
+            displayHand(game.getPlayer());
             getInfo();
         }
         if (game.getPlayer().handCount() == 5) {
@@ -71,6 +112,7 @@ public class GameActivity extends AppCompatActivity {
 
     public void compare(View button) {
         computerScore.setText(Integer.toString(game.getComputer().handValue()));
+        displayHand(game.getComputer());
 
         hitButton.setVisibility(View.GONE);
         compareButton.setVisibility(View.GONE);
@@ -85,6 +127,20 @@ public class GameActivity extends AppCompatActivity {
         String name = playerName.getText().toString();
         intent.putExtra("name", name);
         startActivity(intent);
+    }
+
+    public void displayHand(Player player) {
+        ArrayList<Card> hand = player.getHand();
+        for(int i = 0; i < hand.size(); i++) {
+            String imagePath = hand.get(i).getImage();
+            int resource = getResources().getIdentifier(imagePath, "drawable", this.getPackageName());
+            if(player.getName().equals("Android")) {
+                computerCards.get(i).setImageResource(resource);
+            } else {
+                playerCards.get(i).setImageResource(resource);
+            }
+        }
+
     }
 
 }
